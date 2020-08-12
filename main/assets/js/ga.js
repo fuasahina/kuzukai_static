4d97e857367360af5d08b04d11f3ed1a46298c08
fuasahina <asahina.fumiki@gmail.com>
2020-08-12 10:56:03 +0900

    add gtag

---
+++ main/assets/js/ga.js
@@ -0,0 +1,9 @@
+
+//ファイル名は好きに変更可能です
+//UA-123456789-1のところ2か所を自分のトラッキングIDに変更してください
+document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=UA-175012863-1"></script>');
+window.dataLayer = window.dataLayer || [];
+function gtag(){
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());gtag('config', 'UA-175012863-1');
